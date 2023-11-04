6baeb841cb039120648cdfbc16ac5531fbedc5cc
Jordan Topp <toppdat@gmail.com>
2023-11-04 11:29:16 -0700

    Upated JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-document.querySelector('button').addEventListener('click', () => {
-  console.log('clicked')
-})
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest() {
+  const name = document.querySelector('input').value
+  try {
+    const response = await fetch(
+      `https://spectrum-assembly-api.onrender.com/${name}`
+    )
+    const data = await response.json()
+
+    document.querySelector('h2').innerText = data.email
+  } catch (error) {
+    console.log(error)
+  }
+}
