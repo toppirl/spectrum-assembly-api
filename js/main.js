81b894442e692b224fc6bf0beb2675dffb50b60e
Jordan Topp <toppdat@gmail.com>
2023-11-04 13:59:50 -0700

    updated fetch route with /api

--- js/main.js
+++ js/main.js
@@ -7,10 +7,9 @@ async function apiRequest() {
   const name = document.querySelector('input').value
   try {
     const response = await fetch(
-      `https://spectrum-assembly-api.onrender.com/${name}`
+      `https://spectrum-assembly-api.onrender.com/api/${name}`
     )
     const data = await response.json()
-    console.log(data)
     document.querySelector('h2').innerText = data.email
   } catch (error) {
     console.log(error)
